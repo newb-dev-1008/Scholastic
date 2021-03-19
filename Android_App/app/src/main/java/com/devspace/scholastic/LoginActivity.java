2bf05b6e6bf80ecb7abf8bbc7dd4eac2ac9fcf71
Yash Umale <yashumale223@gmail.com>
2021-03-20 02:17:33 +0530

    Signup arrangements

--- Android_App/app/src/main/java/com/devspace/scholastic/LoginActivity.java
+++ Android_App/app/src/main/java/com/devspace/scholastic/LoginActivity.java
@@ -11,6 +11,7 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -27,15 +28,16 @@ import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText emailET, nameET, passwordET, confPassET, phoneET;
+    private EditText emailET, nameET, passwordET, confPassET, phoneET, signInPW;
     private RadioGroup userType;
     private RadioButton userTypeSelected;
     private Button enterMail, signUpBtn;
-    private TextView dateOfBirth, studentYearTV;
+    private TextView dateOfBirth, dateOfBirthET, studentYearTV, userProfileTV;
     private FirebaseAuth firebaseAuth;
     private FirebaseFirestore db;
     private Spinner studentYear;
     private FirebaseAuth.AuthStateListener authStateListener;
+    private int enterFlag = 1;
     private DatePickerDialog.OnDateSetListener dateSetListener; // Needs to be completed
 
     private String UIDEmailID;
@@ -54,8 +56,11 @@ public class LoginActivity extends AppCompatActivity {
         enterMail = findViewById(R.id.signupEnterEmailButton);
         signUpBtn = findViewById(R.id.signupButton);
         dateOfBirth = findViewById(R.id.dateOfBirth);
+        dateOfBirthET = findViewById(R.id.dateOfBirthET);
         studentYearTV = findViewById(R.id.studentSemTV);
         studentYear = findViewById(R.id.studentYearSpinner);
+        signInPW = findViewById(R.id.signinPassword);
+        userProfileTV = findViewById(R.id.userType);
 
         firebaseAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
@@ -63,22 +68,43 @@ public class LoginActivity extends AppCompatActivity {
         enterMail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(enterMail.getWindowToken(), 0);
-                String EmailID = emailET.getText().toString().trim();
-                UIDEmailID = EmailID;
-                if (isEmailValid(UIDEmailID)) {
-                    firebaseAuth.fetchSignInMethodsForEmail(UIDEmailID).addOnCompleteListener(new OnCompleteListener<SignInMethodQueryResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<SignInMethodQueryResult> task) {
-                            if (task.isSuccessful()) {
-                                List<String> signInMethods = task.getResult().getSignInMethods();
-                                if (signInMethods.size() != 0) {
-                                    emailET
+                if (enterFlag == 1) {
+                    enterFlag = 2;
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(enterMail.getWindowToken(), 0);
+                    String EmailID = emailET.getText().toString().trim();
+                    UIDEmailID = EmailID;
+                    if (isEmailValid(UIDEmailID)) {
+                        firebaseAuth.fetchSignInMethodsForEmail(UIDEmailID).addOnCompleteListener(new OnCompleteListener<SignInMethodQueryResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<SignInMethodQueryResult> task) {
+                                if (task.isSuccessful()) {
+                                    List<String> signInMethods = task.getResult().getSignInMethods();
+                                    if (signInMethods.size() != 0) {
+                                        signInPW.setVisibility(View.VISIBLE);
+                                        enterMail.setText("Sign In");
+                                    } else {
+                                        Toast.makeText(LoginActivity.this, "You haven't signed up yet. Sign up now!", Toast.LENGTH_SHORT).show();
+                                        emailET.setVisibility(View.GONE);
+                                        enterMail.setVisibility(View.GONE);
+                                        nameET.setVisibility(View.VISIBLE);
+                                        passwordET.setVisibility(View.VISIBLE);
+                                        confPassET.setVisibility(View.VISIBLE);
+                                        phoneET.setVisibility(View.VISIBLE);
+                                        dateOfBirth.setVisibility(View.VISIBLE);
+                                        dateOfBirthET.setVisibility(View.VISIBLE);
+                                        userProfileTV.setVisibility(View.VISIBLE);
+                                        userType.setVisibility(View.VISIBLE);
+                                        studentYearTV.setVisibility(View.VISIBLE);
+                                        studentYear.setVisibility(View.VISIBLE);
+                                        signUpBtn.setVisibility(View.VISIBLE);
+                                    }
                                 }
                             }
-                        }
-                    })
+                        });
+                    }
+                } else {
+
                 }
             }
         });
