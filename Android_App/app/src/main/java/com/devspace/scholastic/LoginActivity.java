43af5dd74973fe08df80b88dc60c063df4ceff8d
Yash Umale <yashumale223@gmail.com>
2021-03-20 03:24:00 +0530

    Final commit for [3:20am, 20th Mar]

--- Android_App/app/src/main/java/com/devspace/scholastic/LoginActivity.java
+++ Android_App/app/src/main/java/com/devspace/scholastic/LoginActivity.java
@@ -2,6 +2,7 @@ package com.devspace.scholastic;
 
 import android.app.DatePickerDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -138,7 +139,8 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void signUp() {
-        // Finish this
+        // Finish the Firestore part
+
         String password = confPassET.getText().toString().trim();
         firebaseAuth.createUserWithEmailAndPassword(UIDEmailID, password)
                 .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
