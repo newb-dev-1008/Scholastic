90b916d546c240abd1004bfd2ddb5ad7ef7220b2
Yash Umale <yashumale223@gmail.com>
2021-03-21 00:51:27 +0530

    Classes and Virtual AR Labs - 2

--- Android_App/app/src/main/java/com/devspace/scholastic/LabClassesFragment.java
+++ Android_App/app/src/main/java/com/devspace/scholastic/LabClassesFragment.java
@@ -4,16 +4,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.button.MaterialButton;
+
 public class LabClassesFragment extends Fragment {
+
+    private MaterialButton arLab, joinClass;
+    private TextView classStatus;
+    private View root;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.labs_classes, container, false);
+        root = inflater.inflate(R.layout.labs_classes, container, false);
+
+        arLab = root.findViewById(R.id.virtualLab);
+        joinClass = root.findViewById(R.id.joinClass);
+        classStatus = root.findViewById(R.id.classStatusTV);
+
+        return root;
     }
 }
