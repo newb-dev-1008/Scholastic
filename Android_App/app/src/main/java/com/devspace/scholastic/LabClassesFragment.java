e55713945e43380e6156de3f8f6b62cc527bdfd8
Yash Umale <yashumale223@gmail.com>
2021-03-20 15:50:42 +0530

    LabClassFragment

---
+++ Android_App/app/src/main/java/com/devspace/scholastic/LabClassesFragment.java
@@ -0,0 +1,19 @@
+package com.devspace.scholastic;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class LabClassesFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.labs_classes, container, false);
+    }
+}
