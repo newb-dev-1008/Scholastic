a9c534fb8094a1b7919dd4cb36ce946ab8d42cc3
Yash Umale <yashumale223@gmail.com>
2021-03-21 04:19:45 +0530

    Plagiarism Checker - 5 (FilePicker)

--- Android_App/app/src/main/java/com/devspace/scholastic/PlagiarismCheckFragment.java
+++ Android_App/app/src/main/java/com/devspace/scholastic/PlagiarismCheckFragment.java
@@ -1,5 +1,7 @@
 package com.devspace.scholastic;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +14,8 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 public class PlagiarismCheckFragment extends Fragment {
@@ -20,6 +24,12 @@ public class PlagiarismCheckFragment extends Fragment {
     private MaterialButton upload1, upload2, simCheckBtn;
     private TextView plagDescTV, upload1TV, upload2TV, simResult;
     private ProgressBar simProg;
+    private Uri fileUri;
+    private String filePath;
+    private FirebaseFirestore db;
+    private FirebaseAuth firebaseAuth;
+
+    public static final int PICKFILE_RESULT_CODE = 1;
 
     @Nullable
     @Override
@@ -34,6 +44,31 @@ public class PlagiarismCheckFragment extends Fragment {
         simResult = root.findViewById(R.id.simScoreTV);
         simProg = root.findViewById(R.id.plagProgress);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
+        upload1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent chooseFile = new Intent(Intent.ACTION_GET_CONTENT);
+                chooseFile.setType("*/*");
+                chooseFile = Intent.createChooser(chooseFile, "Choose a file");
+                startActivityForResult(chooseFile, PICKFILE_RESULT_CODE);
+            }
+        });
+
         return root;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case PICKFILE_RESULT_CODE:
+                if (resultCode == -1) {
+                    fileUri = data.getData();
+                    filePath = fileUri.getPath();
+                }
+                break;
+        }
     }
 }
