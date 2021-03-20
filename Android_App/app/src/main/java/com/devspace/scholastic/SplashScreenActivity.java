e2a2e6b052d1ce528923a8812372aae2ae902cc3
Yash Umale <yashumale223@gmail.com>
2021-03-20 18:22:55 +0530

    NullPointerExceptions fixed (SplashScreenActivity)

--- Android_App/app/src/main/java/com/devspace/scholastic/SplashScreenActivity.java
+++ Android_App/app/src/main/java/com/devspace/scholastic/SplashScreenActivity.java
@@ -3,16 +3,24 @@ package com.devspace.scholastic;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore db;
+    private String userType;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
@@ -20,24 +28,44 @@ public class SplashScreenActivity extends AppCompatActivity {
         setContentView(R.layout.splashscreen);
 
         firebaseAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
-        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
-        if (currentUser != null) {
+        try {
+            FirebaseUser currentUser = firebaseAuth.getCurrentUser();
             moveForth();
-        } else {
+        } catch (NullPointerException e) {
             Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent);
         }
+
     }
 
     private void moveForth() {
-        // finish this
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        db.collection("Users").document("User " + currentUser.getEmail())
+                .get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                userType = documentSnapshot.get("userType").toString();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(SplashScreenActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Intent intent = new Intent(SplashScreenActivity.this, WelcomeNavbarActivity.class);
+        intent.putExtra("userType", userType);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
