80763d0e28f4279ebf3e153d6fea23788e3965a2
Yash Umale <yashumale223@gmail.com>
2021-03-21 05:30:50 +0530

    Home Fragment - 2

---
+++ Android_App/app/src/main/java/com/devspace/scholastic/WelcomeScreenFragment.java
@@ -0,0 +1,59 @@
+package com.devspace.scholastic;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class WelcomeScreenFragment extends Fragment {
+
+    private RecyclerView deadlinesRV, eventsRV, announcementsRV;
+    private String userType;
+    private TextView userName;
+    private View root;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore db;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        root = inflater.inflate(R.layout.home_screen, container, false);
+        firebaseAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
+        userName = root.findViewById(R.id.homeNameTV);
+
+        db.collection("Users").document(firebaseAuth.getCurrentUser().getEmail()).get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        String name = documentSnapshot.get("fullName").toString();
+                        userName.setText(name);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return root;
+    }
+
+}
